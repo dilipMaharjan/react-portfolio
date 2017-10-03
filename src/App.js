cd3044c29e4dee49c28f2f6a891308d3553a1767
dilipMaharjan <dilipmaharjann@gmail.com>
2017-10-03 16:39:01 -0400

    semantic ui configured

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Button } from 'semantic-ui-react'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+<Button>Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
