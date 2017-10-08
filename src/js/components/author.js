da48a1ae3bacb128fdb48b8f953ba623a29a52fe
dmaharjan <dilipmaharjann@gmail.com>
2017-10-07 22:21:02 -0400

    author image centered

--- src/js/components/author.js
+++ src/js/components/author.js
@@ -1,8 +1,26 @@
 
 import React from 'react'
-import { Image } from 'semantic-ui-react'
+import { Image, Container, Header, Table } from 'semantic-ui-react'
+import author from '../../images/author.jpg';
 
 const Author = () => (
-  <Image src='../../images/author.jpg' size='medium' shape='circular' />
+  <Container fluid centered>
+    <Image src={author} alt="Image not found" size='medium' shape='circular' centered />
+    <Header as='h1' size='huge' color='blue'>Dilip Maharjan</Header>
+    <Table basic='very' centered>
+        <Table.Row>
+          <Table.HeaderCell textAlign='center'>
+            <Header as='h2' size='huge' color='blue'>Software Developer</Header></Table.HeaderCell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>
+            <Header as='h2' size='large' color='blue'>Mobile Application Developer</Header>
+          </Table.Cell>
+          <Table.Cell>
+            <Header as='h2' size='large' color='blue'>Web Application Developer</Header>
+          </Table.Cell>
+        </Table.Row>
+    </Table>
+  </Container>
 )
 export default Author;
---
+++ src/js/components/container.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+
+class Container extends Component{
+    render(){
+        
+    }
+}
