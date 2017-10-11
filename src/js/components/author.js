5484810dcd0fdff4eec364244a3878702afe0c64
dmaharjan <dilipmaharjann@gmail.com>
2017-10-10 22:28:00 -0400

    added branching

--- src/js/components/author.js
+++ src/js/components/author.js
@@ -1,6 +1,6 @@
 
-import React from 'react'
-import { Image, Container, Header, Table } from 'semantic-ui-react'
+import React from 'react';
+import { Image, Container, Header, Table, Icon, Segment } from 'semantic-ui-react';
 import author from '../../images/author.jpg';
 
 const Author = () => (
@@ -8,19 +8,27 @@ const Author = () => (
     <Image src={author} alt="Image not found" size='medium' shape='circular' centered />
     <Header as='h1' size='huge' color='blue'>Dilip Maharjan</Header>
     <Table basic='very' centered>
-        <Table.Row>
-          <Table.HeaderCell textAlign='center'>
-            <Header as='h2' size='huge' color='blue'>Software Developer</Header></Table.HeaderCell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>
-            <Header as='h2' size='large' color='blue'>Mobile Application Developer</Header>
-          </Table.Cell>
-          <Table.Cell>
-            <Header as='h2' size='large' color='blue'>Web Application Developer</Header>
-          </Table.Cell>
-        </Table.Row>
+      <Table.Row className='software'>
+        <Table.HeaderCell>
+          <Header as='h2' size='huge' color='blue'>Software Developer</Header></Table.HeaderCell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>
+          <Header as='h2' size='large' color='blue'>Mobile Application Developer</Header>
+        </Table.Cell>
+        <Table.Cell>
+          <Header as='h2' size='large' color='blue'>Web Application Developer</Header>
+        </Table.Cell>
+      </Table.Row>
     </Table>
+    <Container>
+      <Header size='huge' color='blue'>Find me <Icon name='at' /></Header>
+      <Icon size='huge' color='blue' name='linkedin' />
+      <Icon size='huge' color='blue' name='github' />
+    </Container>
+    <Container textAlign='center'>
+      <Header><Icon color='blue' name='copyright' />{new Date().getFullYear()}</Header>
+    </Container>
   </Container>
 )
 export default Author;
